98f4652a6eb29d05fa1a015c9a73221461f20d14
Ryan Pielow <Ryan.Pielow@gmail.com>
2023-11-26 19:35:17 -0800

    update

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -5,6 +5,10 @@ M.ui = {
   theme = 'github_dark',
   theme_toggle = { 'github_dark', 'github_light' },
 
+ 	-- highlight groups!
+	hl_add = require("custom.highlights").new_hlgroups,
+	hl_override = require("custom.highlights").overridden_hlgroups,
+
   transparency = true,
 }
 
